ed60b95a4827bead06f36fe3d3d7f42b0278d8b6
jimmy <jimmyjt@gmail.com>
2023-07-05 12:10:04 -0500

    Adding various reporting levels

--- uvm_report_macros/testbench.sv
+++ uvm_report_macros/testbench.sv
@@ -6,9 +6,14 @@ module tb;
 
   `include "uvm_macros.svh"
   initial begin
+    // Trying various report macro levels
     `uvm_info("Test 1","Hello World", UVM_NONE);
-    uvm_report_info("TEST 2", "This is Reporting", UVM_MEDIUM);
-    uvm_report_info("TEST 2", "This is Reporting", UVM_FULL);
+    `uvm_info("Test 2","Hello World", UVM_LOW);
+    `uvm_info("Test 3","Hello World", UVM_MEDIUM);
+    `uvm_info("Test 4","Hello World", UVM_HIGH);
+    `uvm_info("Test 5","Hello World", UVM_FULL);
+    `uvm_info("Test 1","Hello World", UVM_DEBUG);
+    uvm_report_info("TEST 2", "This is Reporting MEDIUM", UVM_MEDIUM);
     `uvm_error("BAD", "This is an error");
     `uvm_warning("W", "This is a warning");
     `uvm_fatal("MOD", "This is Fatal");
